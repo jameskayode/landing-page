d960c50e81967d150818251cd8e3f961b7e3794f
James Kayode <Jameskayode39@gmail.com>
2023-03-24 09:45:04 +0100

    next

--- qr.js
+++ qr.js
@@ -3,48 +3,48 @@
 // const customerDetailsButton = document.querySelector('#customer-details button[type="submit"]');
 // const paymentButton = document.querySelector('#payment button[type="submit"]');
 
-const cardNumberInput = document.querySelector('#card-number');
-const expiryDateInput = document.querySelector('#expiry-date');
-const cvvInput = document.querySelector('#cvv');
-const accountNumberInput = document.querySelector('#account-number');
-const lastPaymentInput = document.querySelector('#last-payment');
-const addressInput = document.querySelector('#address');
-const fullNameInput = document.querySelector('#full-name');
+// const cardNumberInput = document.querySelector('#card-number');
+// const expiryDateInput = document.querySelector('#expiry-date');
+// const cvvInput = document.querySelector('#cvv');
+// const accountNumberInput = document.querySelector('#account-number');
+// const lastPaymentInput = document.querySelector('#last-payment');
+// const addressInput = document.querySelector('#address');
+// const fullNameInput = document.querySelector('#full-name');
 
 // get the elements
-const paymentCustomer = document.querySelector('.big');
-const customerDetails = document.querySelector('#customer');
-const paymentDetails = document.querySelector('#payment');
-const showCustomerBtn = document.querySelector('#showCustomer');
-const showPaymentBtn = document.querySelector('#showPayment');
-const customerForm = document.querySelector('#customerDetails');
-const paymentForm = document.querySelector('#paymentDetails');
+// const paymentCustomer = document.querySelector('.big');
+// const customerDetails = document.querySelector('#customer');
+// const paymentDetails = document.querySelector('#payment');
+// const showCustomerBtn = document.querySelector('#showCustomer');
+// const showPaymentBtn = document.querySelector('#showPayment');
+// const customerForm = document.querySelector('#customerDetails');
+// const paymentForm = document.querySelector('#paymentDetails');
 
 
 // show customer details by default
-customerDetails.classList.remove('hidden');
-customerForm.classList.remove('hidden');
+// customerDetails.classList.remove('hidden');
+// customerForm.classList.remove('hidden');
 
 // add click event listeners to buttons
-showCustomerBtn.addEventListener('click', function() {
-  customerDetails.classList.remove('hidden');
-  paymentDetails.classList.add('hidden');
-  customerForm.classList.remove('hidden');
-  paymentForm.classList.add('hidden');
-});
-showPaymentBtn.addEventListener('click', function() {
-    customerDetails.classList.add('hidden');
-    paymentDetails.classList.remove('hidden');
-    customerForm.classList.add('hidden');
-    paymentForm.classList.remove('hidden');
-  });
+// showCustomerBtn.addEventListener('click', function() {
+//   customerDetails.classList.remove('hidden');
+//   paymentDetails.classList.add('hidden');
+//   customerForm.classList.remove('hidden');
+//   paymentForm.classList.add('hidden');
+// });
+// showPaymentBtn.addEventListener('click', function() {
+//     customerDetails.classList.add('hidden');
+//     paymentDetails.classList.remove('hidden');
+//     customerForm.classList.add('hidden');
+//     paymentForm.classList.remove('hidden');
+//   });
   
   // make the payment and customer details big on page load
-  paymentCustomer.classList.add('big');
-  paymentCustomer.classList.remove('hidden');
+  // paymentCustomer.classList.add('big');
+  // paymentCustomer.classList.remove('hidden');
   
   // hide the buttons on page load
-  document.querySelector('#buttons').classList.add('hidden');
+  // document.querySelector('#buttons').classList.add('hidden');
 
 // Define the event listeners
 // customerDetailsButton.addEventListener('click', showCustomerDetails);
